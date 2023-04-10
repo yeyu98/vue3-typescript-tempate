31fcaef180e06e686dc24430f44d7395f496ede6
xiaohu <zhouyi@yx.com>
2023-04-10 14:27:54 +0800

    feat: 处理vue导入error

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+// TODO 这里目前不知道什么意思后续查
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
