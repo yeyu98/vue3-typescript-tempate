ccc01750628bed93985f5e88fe1dc974e886b6c4
xiaohu <zhouyi@yx.com>
2023-04-10 16:18:13 +0800

    feat: 自动启动配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: lzy-Jerry
  * @Date: 2023-03-20 22:12:44
  * @LastEditors: xiaohu
- * @LastEditTime: 2023-04-10 14:52:40
+ * @LastEditTime: 2023-04-10 16:17:15
  * @Description:
  */
 import { defineConfig } from 'vite';
@@ -27,5 +27,6 @@ export default defineConfig({
   },
   server: {
     port: 8080,
+    open: true,
   },
 });
